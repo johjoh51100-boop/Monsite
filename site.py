34fcfd53df01077d494a2843b68f59b0dd2164ac
johjoh51100-boop <johjoh51100@gmail.com>
2026-01-01 20:57:43 +0100

    Update site.py

--- site.py
+++ site.py
@@ -1,23 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+import os
+from datetime import datetime
 
 app = Flask(__name__)
+SECRET_PASSWORD = "1234"
 
 @app.route("/", methods=["GET", "POST"])
-def index():
+def login():
     message = ""
-
     if request.method == "POST":
-        nom = request.form.get("nom")
-        message = f"Bonjour {nom} 👋"
+        password = request.form.get("password")
+        if password == SECRET_PASSWORD:
+            user_ip = request.remote_addr
+            now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            return render_template("success.html", ip=user_ip, date=now)
+        else:
+            message = "❌ Mot de passe incorrect"
+    return render_template("index.html", message=message)
 
-    return f"""
-    <h1>Mon site en ligne</h1>
-    <form method="POST">
-        <input name="nom">
-        <button>Envoyer</button>
-    </form>
-    <p>{message}</p>
-    """
-
-# ⚠️ IMPORTANT
-app.run(host="0.0.0.0", port=10000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
